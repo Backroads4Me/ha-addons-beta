be2a9df4d523b6d416627623e38ed7f5bd9093cc
Ted Lanham <tedlanham@gmail.com>
2026-02-15 02:44:07 -0500

    build

--- librecoach/run.sh
+++ librecoach/run.sh
@@ -427,7 +427,7 @@ get_managed_version() {
 }
 
 # Ensure this addon starts on boot (upgrades from older versions may have boot: manual)
-api_call POST "/addons/self/options" '{"boot":"auto"}' > /dev/null
+api_call POST "/addons/self/options" '{"boot":"auto","watchdog":true}' > /dev/null
 
 # ========================
 # Phase 0: Deployment
